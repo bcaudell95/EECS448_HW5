8992a0f58a5c5c53abef55697d5c81080e60c8e1
unknown <fv3411@L76MCI0U4259QNZ.cead.prd>
2015-04-20 18:00:21 -0500

    Classification methods skeleton

---
+++ src/ClassificationMethods.java
@@ -0,0 +1,31 @@
+public class ClassificationMethods {
+    public static double EuclidianDistanceMethod() {
+        double activeRatio = 0;
+
+        return activeRatio;
+    }
+
+    public static double MahalanokiDistanceMethod() {
+        double activeRatio = 0;
+
+        return activeRatio;
+    }
+
+    public static double EuclidianVotingMethod() {
+        double activeRatio = 0;
+
+        return activeRatio;
+    }
+
+    public static double MahalanokiVotingMethod() {
+        double activeRatio = 0;
+
+        return activeRatio;
+    }
+
+    public static double CustomMethod() {
+        double activeRatio = 0;
+
+        return activeRatio;
+    }
+}
